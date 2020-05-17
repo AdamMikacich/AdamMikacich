3c268cf3195f8f2d9de1aa722e8351e118bd4866
Adam Mikacich <adammikacich@gmail.com>
2020-05-16 23:08:55 -0700

    Remove unnecessary firebase imports

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,5 @@
 import splash from './splash.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-  try {
-    let app = firebase.app();
-    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-    console.log(`Firebase SDK loaded with ${features.join(', ')}`);
-  } catch (e) {
-    console.error(e);
-    console.error('Error loading the Firebase SDK, check the console.');
-  }
-
   splash();
 });
